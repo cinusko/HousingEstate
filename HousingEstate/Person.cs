bb20b9e29e1ccd9ab3349b9153dafbf82f0ee2e9
cinusko <andrej.bakos0@gmail.com>
2023-01-12 09:15:17 +0100

    added class Person, added variables, added getters and setters for variables

---
+++ HousingEstate/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HousingEstate
+{
+    internal class Person
+    {
+        private string name;
+        private string surname;
+        private int age;
+        
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        
+    }
+}
